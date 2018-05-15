01f63a396eed70526f4359d0891ea74954ab7844
andreabefuen <al341488@uji.es>
2018-05-15 22:08:53 +0200

    Video 2 terminado

---
+++ Tutorial Survival Shooter Final/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //VARIABLES PUBLICAS
+    public float speed = 6f;
+
+
+    //VARIABLES PRIVADAS
+    Vector3 movement; //guarda el movimiento
+    Animator anim;
+    Rigidbody playerRigidbody;
+    int floorMask;
+    float camRayLength = 100f;
+
+    private void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        anim = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+
+        Move(h, v);
+        Turning();
+        Animating(h, v);
+
+    }
+
+    void Move(float h, float v)
+    {
+        movement.Set(h, 0f, v);
+
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        playerRigidbody.MovePosition(transform.position + movement);
+
+    }
+
+    void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit floorHit;
+
+        if ( Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position; //desde el punto en el que golpea el raton a la posición del player
+            playerToMouse.y = 0f;
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidbody.MoveRotation(newRotation);
+        }
+    }
+
+
+    void Animating(float h, float v)
+    {
+        bool walking = h != 0f || v != 0f; //Si hemos pulsado h o v, estamos caminando, sino  no
+        anim.SetBool("IsWalking", walking);
+    }
+
+
+}
