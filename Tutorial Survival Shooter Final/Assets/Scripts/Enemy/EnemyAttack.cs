c181de9ea0d5dfb577a5b1649f0406a3c589ce35
andreabefuen <al341488@uji.es>
2018-05-16 21:12:20 +0200

    Tutorial 6 completo

--- Tutorial Survival Shooter Final/Assets/Scripts/Enemy/EnemyAttack.cs
+++ Tutorial Survival Shooter Final/Assets/Scripts/Enemy/EnemyAttack.cs
@@ -18,12 +18,12 @@ public class EnemyAttack : MonoBehaviour
     void Awake ()
     {
         player = GameObject.FindGameObjectWithTag ("Player");
-        playerHealth = player.GetComponent <PlayerHealth> ();
+        playerHealth = player.GetComponent <PlayerHealth> (); //Para coger el elemento de otra clase
         //enemyHealth = GetComponent<EnemyHealth>();
         anim = GetComponent <Animator> ();
     }
 
-
+    //Cuando entra en el rango
     void OnTriggerEnter (Collider other)
     {
         if(other.gameObject == player)
@@ -32,7 +32,7 @@ public class EnemyAttack : MonoBehaviour
         }
     }
 
-
+    //Cuando sale del rango
     void OnTriggerExit (Collider other)
     {
         if(other.gameObject == player)
@@ -45,12 +45,12 @@ public class EnemyAttack : MonoBehaviour
     void Update ()
     {
         timer += Time.deltaTime;
-
+        //Cuando el tiempo es superior al tiempo entre ataque y el jugador está en rango
         if(timer >= timeBetweenAttacks && playerInRange/* && enemyHealth.currentHealth > 0*/)
         {
             Attack ();
         }
-
+        //Si la salud del jugador cae a cero, la animación de morir ocurre
         if(playerHealth.currentHealth <= 0)
         {
             anim.SetTrigger ("PlayerDead");
